35d93fc675f097022b9b9ce3e1fdc22195bebd29
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-05-12 14:27:33 -0300

    Implementado Cadastro de Fabricante

---
+++ GestaoDeEquipamentos.ConsoleApp/Controllers/ControladorFabricante.cs
@@ -0,0 +1,69 @@
+﻿using GestaoDeEquipamentos.ConsoleApp.Compartilhado;
+using GestaoDeEquipamentos.ConsoleApp.ModuloFabricante;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace GestaoDeEquipamentos.ConsoleApp.Controllers;
+
+[Route("fabricantes")]
+public class ControladorFabricante : Controller
+{
+    [HttpGet("cadastrar")]
+    public Task ExibirFormularioCadastroFabricante()
+    {
+        string conteudo = System.IO.File.ReadAllText("ModuloFabricante/Html/Cadastrar.html");
+
+        return HttpContext.Response.WriteAsync(conteudo);
+    }
+
+    [HttpPost("cadastrar")]
+    public Task CadastrarFabricante()
+    {
+        ContextoDados contextoDados = new ContextoDados(true);
+        IRepositorioFabricante repositorioFabricante = new RepositorioFabricanteEmArquivo(contextoDados);
+
+        string nome = HttpContext.Request.Form["nome"].ToString();
+        string email = HttpContext.Request.Form["email"].ToString();
+        string telefone = HttpContext.Request.Form["telefone"].ToString();
+
+        Fabricante novoFabricante = new Fabricante(nome, email, telefone);
+
+        repositorioFabricante.CadastrarRegistro(novoFabricante);
+
+        string conteudo = System.IO.File.ReadAllText("Compartilhado/Html/Notificacao.html");
+
+        StringBuilder sb = new StringBuilder(conteudo);
+
+        sb.Replace("#mensagem#", $"O registro \"{novoFabricante.Nome}\" foi cadastrado com sucesso!");
+
+        string conteudoString = sb.ToString();
+
+        return HttpContext.Response.WriteAsync(conteudoString);
+    }
+
+    [HttpGet("visualizar")]
+    public IActionResult VisualizarFabricantes()
+    {
+        ContextoDados contextoDados = new ContextoDados(true);
+        IRepositorioFabricante repositorioFabricante = new RepositorioFabricanteEmArquivo(contextoDados);
+
+        string conteudo = System.IO.File.ReadAllText("ModuloFabricante/Html/Visualizar.html");
+
+        StringBuilder stringBuilder = new StringBuilder(conteudo);
+
+        List<Fabricante> fabricantes = repositorioFabricante.SelecionarRegistros();
+
+        foreach (Fabricante f in fabricantes)
+        {
+            string itemLista = $"<li>{f.ToString()} / <a href=\"/fabricantes/editar/{f.Id}\">Editar</a> / <a href=\"/fabricantes/excluir/{f.Id}\">Excluir</a> </li> #fabricante#";
+
+            stringBuilder.Replace("#fabricante#", itemLista);
+        }
+
+        stringBuilder.Replace("#fabricante#", "");
+
+        string conteudoString = stringBuilder.ToString();
+
+        return Content(conteudoString, "text/html");
+    }
+}
--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -17,8 +17,6 @@ class Program
         WebApplication app = builder.Build();
 
         // mapeamento de rotas
-        app.MapGet("/fabricantes/cadastrar", ExibirFormularioCadastroFabricante);
-        app.MapPost("/fabricantes/cadastrar", CadastrarFabricante);
 
         app.MapGet("/fabricantes/editar/{id:int}", ExibirFormularioEdicaoFabricante);
         app.MapPost("/fabricantes/editar/{id:int}", EditarFabricante);
@@ -26,44 +24,13 @@ class Program
         app.MapGet("/fabricantes/excluir/{id:int}", ExibirFormularioExclusaoFabricante);
         app.MapPost("/fabricantes/excluir/{id:int}", ExcluirFabricante);
 
-        app.MapGet("/fabricantes/visualizar", VisualizarFabricantes);
-
         app.MapControllers();
 
         app.Run();
     }
 
-    static Task ExibirFormularioCadastroFabricante(HttpContext context)
-    {
-        string conteudo = File.ReadAllText("ModuloFabricante/Html/Cadastrar.html");
-
-        return context.Response.WriteAsync(conteudo);
-    }
-
     // Controlador de Fabricantes
-    static Task CadastrarFabricante(HttpContext context)
-    {
-        ContextoDados contextoDados = new ContextoDados(true);
-        IRepositorioFabricante repositorioFabricante = new RepositorioFabricanteEmArquivo(contextoDados);
 
-        string nome = context.Request.Form["nome"].ToString();
-        string email = context.Request.Form["email"].ToString();
-        string telefone = context.Request.Form["telefone"].ToString();
-
-        Fabricante novoFabricante = new Fabricante(nome, email, telefone);
-
-        repositorioFabricante.CadastrarRegistro(novoFabricante);
-
-        string conteudo = File.ReadAllText("Compartilhado/Html/Notificacao.html");
-
-        StringBuilder sb = new StringBuilder(conteudo);
-
-        sb.Replace("#mensagem#", $"O registro \"{novoFabricante.Nome}\" foi cadastrado com sucesso!");
-
-        string conteudoString = sb.ToString();
-
-        return context.Response.WriteAsync(conteudoString);
-    }
 
     static Task ExibirFormularioEdicaoFabricante(HttpContext context)
     {
@@ -151,31 +118,6 @@ class Program
         sb.Replace("#mensagem#", $"O registro foi excluído com sucesso!");
 
         string conteudoString = sb.ToString();
-
-        return context.Response.WriteAsync(conteudoString);
-    }
-
-    static Task VisualizarFabricantes(HttpContext context)
-    {
-        ContextoDados contextoDados = new ContextoDados(true);
-        IRepositorioFabricante repositorioFabricante = new RepositorioFabricanteEmArquivo(contextoDados);
-
-        string conteudo = File.ReadAllText("ModuloFabricante/Html/Visualizar.html");
-
-        StringBuilder stringBuilder = new StringBuilder(conteudo);
-
-        List<Fabricante> fabricantes = repositorioFabricante.SelecionarRegistros();
-
-        foreach (Fabricante f in fabricantes)
-        {
-            string itemLista = $"<li>{f.ToString()} / <a href=\"/fabricantes/editar/{f.Id}\">Editar</a> / <a href=\"/fabricantes/excluir/{f.Id}\">Excluir</a> </li> #fabricante#";
-
-            stringBuilder.Replace("#fabricante#", itemLista);
-        }
-
-        stringBuilder.Replace("#fabricante#", "");
-
-        string conteudoString = stringBuilder.ToString();
 
         return context.Response.WriteAsync(conteudoString);
     }
