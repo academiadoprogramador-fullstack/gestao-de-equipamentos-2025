11761624a716f7edb253b5ec01880118c5605055
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-05-13 17:59:58 -0300

    Melhorados comentários da Program.cs

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -4,17 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        // criar um servidor web
+        // Instancia o construtor do servidor...
+        // ...configura as dependências internas (controladores e serviços) e externas (repositórios)
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllersWithViews();
 
+        // Constroi a instância do servidor...
+        // ...configura os middlewares executados durante requisições
         WebApplication app = builder.Build();
 
-        // mapeamento de rotas
+        // Mapeia controladores e rotas que serão gerenciadas pelo servidor
         app.UseRouting();
         app.MapControllers();
 
+        // Inicia o loop do servidor web, escuta por requisições na porta especificada
         app.Run();
     }
 }
