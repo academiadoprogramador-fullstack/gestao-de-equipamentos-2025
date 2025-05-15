867a6f9c246e4a3b96afe63c3a1afa817dee0056
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-05-15 14:30:06 -0300

    Implementada visualização de equipamentos

---
+++ GestaoDeEquipamentos.ConsoleApp/Extensoes/EquipamentoExtensions.cs
@@ -0,0 +1,18 @@
+﻿using GestaoDeEquipamentos.ConsoleApp.Models;
+using GestaoDeEquipamentos.ConsoleApp.ModuloEquipamento;
+
+namespace GestaoDeEquipamentos.ConsoleApp.Extensoes;
+
+public static class EquipamentoExtensions
+{
+    public static DetalhesEquipamentoViewModel ParaDetalhesVM(this Equipamento equipamento)
+    {
+        return new DetalhesEquipamentoViewModel(
+            equipamento.Id,
+            equipamento.Nome,
+            equipamento.PrecoAquisicao,
+            equipamento.DataFabricacao,
+            equipamento.Fabricante.Nome
+        );
+    }
+}
